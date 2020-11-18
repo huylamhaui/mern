24ab541807f0f26cc25938e5abed70530b2b37aa
lamph11 <lamph11@fpt.com.vn>
2020-11-19 01:30:07 +0700

    create layout

---
+++ fe/src/components/BlogNav/index.jsx
@@ -0,0 +1,12 @@
+import './style.css';
+
+function BlogNav() {
+
+    return (
+        <nav className="blog-nav">
+            <ul></ul>
+        </nav>
+    )
+}
+
+export default BlogNav;
---
+++ fe/src/layouts/BlogLayout/index.jsx
@@ -0,0 +1,16 @@
+import BlogNav from '../../components/BlogNav/index';
+import './style.css';
+
+function BlogLayout() {
+
+    return (
+        <div className="blog-layout">
+            <BlogNav />
+            <div className="blog-content">
+                
+            </div>
+        </div>
+    )
+}
+
+export default BlogLayout;
